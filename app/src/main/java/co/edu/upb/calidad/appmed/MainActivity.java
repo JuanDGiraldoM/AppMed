0cd1a58a84db08a3eb64449064c6a3ddc727dcf7
Cristian <cristhian.toro@upb.edu.co>
2018-02-14 01:29:47 -0500

    API Login FB
    Inicialización de implementación de API log in de Facebook.

--- app/src/main/java/co/edu/upb/calidad/appmed/MainActivity.java
+++ app/src/main/java/co/edu/upb/calidad/appmed/MainActivity.java
@@ -2,6 +2,8 @@ package co.edu.upb.calidad.appmed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
     }
 }
