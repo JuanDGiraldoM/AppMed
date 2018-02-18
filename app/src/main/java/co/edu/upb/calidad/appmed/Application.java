fcb286146c56d9a65ff95bda6b736127bf8b07c5
Juan D Giraldo M <judagima@gmail.com>
2018-02-17 22:50:53 -0500

    Sincronización y corrección de errores en GitHub
    Recuperación de archivos
    Recuperación de los últimos commits funcionales

---
+++ app/src/main/java/co/edu/upb/calidad/appmed/Application.java
@@ -0,0 +1,18 @@
+package co.edu.upb.calidad.appmed;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+/**
+ * Created by Cristian on 15/02/2018.
+ */
+
+public class Application extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+    }
+}
--- app/src/main/java/co/edu/upb/calidad/appmed/MainActivity.java
+++ app/src/main/java/co/edu/upb/calidad/appmed/MainActivity.java
@@ -1,9 +1,12 @@
 package co.edu.upb.calidad.appmed;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
+import android.view.View;
+
+import com.facebook.AccessToken;
+import com.facebook.login.LoginManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
+
+        if (AccessToken.getCurrentAccessToken() == null) {
+            goLoginScreen();
+        }
+    }
+
+    private void goLoginScreen() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+    }
+
+    public void logout(View view) {
+        LoginManager.getInstance().logOut();
+        goLoginScreen();
     }
 }
