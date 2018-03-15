0920c3e102fc0d154984f606c266ee982af58f71
Juan D Giraldo M <judagima@gmail.com>
2018-03-15 10:20:05 -0500

    Splash Screen implementada

--- app/src/main/java/co/edu/upb/calidad/appmed/SplashScreen.java
+++ app/src/main/java/co/edu/upb/calidad/appmed/SplashScreen.java
@@ -1,9 +1,9 @@
 package co.edu.upb.calidad.appmed;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -13,7 +13,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
 
         new Handler().postDelayed(new Runnable() {
