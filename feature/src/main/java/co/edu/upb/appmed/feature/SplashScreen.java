9b8983d38bc3d5eac13a13fb9f1c51a5d99b183b
JuanDGiraldoM <juandagiraldoma@gmail.com>
2018-03-28 23:08:59 -0500

    Splash Screen añadido
    Splash Screen actualizado

---
+++ feature/src/main/java/co/edu/upb/appmed/feature/SplashScreen.java
@@ -0,0 +1,36 @@
+package co.edu.upb.appmed.feature;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+/**
+ * Clase que inicia la aplicación
+ */
+public class SplashScreen extends AppCompatActivity {
+
+    /**
+     * Método constructor
+     * @param savedInstanceState savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
