c78f634186401ab302c4623d8cdf8707aac514a7
JuanDGiraldoM <juandagiraldoma@gmail.com>
2018-04-19 10:19:11 -0500

    SitesListActivity actualizado
    Se añadió un SiteHolder y se añadieron algunos xml para el diseño

---
+++ feature/src/main/java/co/edu/upb/appmed/feature/Holders/SiteHolder.java
@@ -0,0 +1,17 @@
+package co.edu.upb.appmed.feature.Holders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import co.edu.upb.appmed.feature.R;
+
+public class SiteHolder extends RecyclerView.ViewHolder {
+
+    private View view;
+
+    public SiteHolder(View itemView) {
+        super(itemView);
+        this.view = itemView;
+    }
+}
--- feature/src/main/java/co/edu/upb/appmed/feature/SitesListActivity.java
+++ feature/src/main/java/co/edu/upb/appmed/feature/SitesListActivity.java
@@ -1,13 +1,67 @@
 package co.edu.upb.appmed.feature;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import co.edu.upb.appmed.feature.Utilities.Utilities;
 
 public class SitesListActivity extends AppCompatActivity {
+
+    FirebaseRecyclerAdapter firebaseRecyclerAdapter;
+    private int activityCaller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sites_list);
+
+        Bundle data = this.getIntent().getExtras();
+        activityCaller = data.getInt("activity");
+
+        switch (activityCaller) {
+            case Utilities.HOSPITALS:
+                Query dbHospitals = FirebaseDatabase.getInstance().getReference()
+                        .child("3")
+                        .orderByChild("nombre_sede");
+
+                RecyclerView recycler = findViewById(R.id.sites_list);
+                recycler.setHasFixedSize(true);
+                recycler.setLayoutManager(new LinearLayoutManager(this));
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        firebaseRecyclerAdapter.cleanup();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(co.edu.upb.appmed.R.menu.menu_place, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case co.edu.upb.appmed.R.id.control_map:
+                Intent intent_maps = new Intent(this, MapsActivity.class);
+                startActivity(intent_maps);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
