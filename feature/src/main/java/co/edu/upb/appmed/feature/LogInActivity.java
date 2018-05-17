1907864bc2dbc7ba6e384a857fabec700cae5421
Cristian <cristhian.toro@upb.edu.co>
2018-05-16 22:28:12 -0500

    login Google

--- feature/src/main/java/co/edu/upb/appmed/feature/LogInActivity.java
+++ feature/src/main/java/co/edu/upb/appmed/feature/LogInActivity.java
@@ -1,8 +1,11 @@
 package co.edu.upb.appmed.feature;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
@@ -10,19 +13,40 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-public class LogInActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
+    SignInButton googleLogIn;
     private LoginButton loginButton;
     private CallbackManager callbackManager;
+    GoogleApiClient mGoogleSignInClient;
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+                .build();
+
         callbackManager = CallbackManager.Factory.create();
         loginButton = (LoginButton) findViewById(R.id.loginButton);
+        googleLogIn = (SignInButton) findViewById(R.id.singInGoogle);
+        googleLogIn.setOnClickListener(this);
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
@@ -54,6 +78,35 @@ public class LogInActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==RC_SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSign(result);
+        }else
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+
     }
+
+    private void handleSign(GoogleSignInResult result){
+        Log.d(TAG,"handleSignInResult:" + result.isSuccess());
+        if(result.isSuccess()){
+            GoogleSignInAccount acct = result.getSignInAccount();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        signIn();
+
+    }
+
+    private void signIn(){
+        Intent signInGooIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleSignInClient);
+        startActivityForResult(signInGooIntent,RC_SIGN_IN);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
 }
